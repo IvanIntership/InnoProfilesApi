7868f0a262f433e9881eae08c387ee7dd4981873
lapivan <lapunoujohn@gmail.com>
2026-07-16 10:13:27 +0300

    Add UnitOfWork interface and implementation

---
+++ ProfilesApi/ProfilesApi.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace ProfilesApi.Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    IAccountRepository Accounts { get; }
+    IAdministratorRepository Administrators { get; }
+    IDoctorRepository Doctors { get; }
+    IOfficeRepository Offices { get; }
+    IPatientRepository Patient { get; }
+    IPhotoRepository Photos { get; }
+    ISpecializationRepository Specializations { get; }
+
+    Task<int> CompleteAsync();
+}
