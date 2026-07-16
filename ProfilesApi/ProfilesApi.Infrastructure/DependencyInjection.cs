5dec77eee924b6393f5b837ab32b15be31dfc109
lapivan <lapunoujohn@gmail.com>
2026-07-16 10:31:06 +0300

    Add forgotten registration of UoW

--- ProfilesApi/ProfilesApi.Infrastructure/DependencyInjection.cs
+++ ProfilesApi/ProfilesApi.Infrastructure/DependencyInjection.cs
@@ -21,6 +21,7 @@ public static class DependencyInjection
         services.AddScoped<IPatientRepository, PatientRepository>();
         services.AddScoped<IPhotoRepository, PhotoRepository>();
         services.AddScoped<ISpecializationRepository, SpecializationRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         
         return services;
     }
