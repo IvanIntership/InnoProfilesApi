35b734711fd986d929f494795ab531f7ee54cfbf
lapivan <lapunoujohn@gmail.com>
2026-07-17 16:04:35 +0300

    Add mapper registration

--- ProfilesApi/ProfilesApi.Application/DependencyInjection.cs
+++ ProfilesApi/ProfilesApi.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using ProfilesApi.Application.Dto.Accounts;
+using ProfilesApi.Application.Mappings;
 
 namespace ProfilesApi.Application;
 
@@ -9,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddValidatorsFromAssemblyContaining<AccountDto>();
+        services.AddAutoMapper(typeof(AccountMappingProfile).Assembly);
         
         return services;
     }
